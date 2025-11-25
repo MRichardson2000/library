2bbc835c3246d7f90c98396884d43e823578bc71
Marcus <marcusrichardson7838@gmail.com>
2025-11-25 16:48:01 +0000

    Pytest for testing get user func

---
+++ tests/test_get_user.py
@@ -0,0 +1,12 @@
+from data.classes import Loan, User
+from tests.test_add_book import test_add_book
+from datetime import datetime
+
+
+def test_get_user() -> None:
+    book = test_add_book()
+    user = User(1, "user", "user", "user@user.user.user", 73849043912)
+    loan = Loan(book, user)
+    loan.borrow_book(now=datetime.now())
+    get_user = loan.get_user()
+    assert get_user == (user.first_name, user.last_name)
