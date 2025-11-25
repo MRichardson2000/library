936ddb8cd27341bb595c9ee8753fdf3e46d88d3e
Marcus <marcusrichardson7838@gmail.com>
2025-11-25 11:33:18 +0000

    Created test for updating rating

--- tests/test_add_book.py
+++ tests/test_add_book.py
@@ -5,17 +5,18 @@ from data.classes import Book
 from data.models import book_insert
 
 
-def test_add_book(db: DB) -> None:
-    db = load_env(testing=True)
+def test_add_book(db: DB = load_env(testing=True)) -> Book:
+    execute_query("ALTER SEQUENCE library_test_unique_id_seq RESTART WITH 1;")
     book = Book(1, "test", "test", "test", 3)
     execute_query(book_insert, params=book.__dict__, db_details=db)
     output = execute_query(
         """
         select *
-        from library_test
+        from book
         where title = 'test'
         """
     )
     assert output
     assert book.title == "test"
     assert output == book
+    return book
---
+++ tests/test_update_rating.py
@@ -0,0 +1,12 @@
+from tests.test_add_book import test_add_book
+from data.classes import Book
+
+
+new_book = test_add_book()
+old_rating = new_book.rating
+new_rating = Book.update_rating(new_book, new_rating=5)
+
+assert new_rating
+assert old_rating != new_rating
+assert new_rating == 5
+assert isinstance(new_rating, int | float)
