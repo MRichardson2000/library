a62c4f25ff4ef57d2b3ed6da1de6b897449c774b
Marcus <marcusrichardson7838@gmail.com>
2025-11-25 16:20:59 +0000

    Refactored test get due date function

--- tests/test_borrow_book.py
+++ tests/test_borrow_book.py
@@ -1,12 +1,11 @@
 from data.classes import Loan, User
 from tests.test_add_book import test_add_book
-from datetime import datetime
 
 
 def test_borrow_book() -> None:
     book = test_add_book()
     user = User(1, "user", "user", "user@user.user.user", 73849043912)
     loan = Loan(book, user)
-    loan.borrow_book(now=datetime.now())
+    loan.borrow_book()
     assert loan.loaned == True
     assert user.first_name == "user"
