f42b428443b5ba2a9b5ada2b33a0e000e1a69d0e
Marcus <marcusrichardson7838@gmail.com>
2025-11-28 16:44:41 +0000

    Commented out inventory services for now, coming back to this later

--- src/services/inventory_services.py
+++ src/services/inventory_services.py
@@ -1,27 +1,26 @@
-from __future__ import annotations
-from data.classes.inventory import Inventory
-from data.classes.book import Book
-from data.database.dbconn import execute_query
-from data.database.models import inventory_insert
-from typing import Any
+# from __future__ import annotations
+# from data.classes.inventory import Inventory
+# from data.classes.book import Book
+# from data.database.dbconn import execute_query
+# from data.database.models import inventory_insert
+# from typing import Any
 
 
-class InventoryServices:
-    def __init__(self) -> None:
-        pass
+# class InventoryServices:
+#     def __init__(self) -> None:
+#         pass
 
-    # def add_stock(self, amount: int, book: Book) -> bool:
-    #     """
-    #     .
+#     def add_stock(self, amount: int, book: Book) -> None:
+#         """
+#         .
 
-    #     Args:
+#         Args:
 
-    #     Returns:
+#         Returns:
 
-    #     Raises:
+#         Raises:
 
-    #     Notes:
+#         Notes:
 
-    #     """
-    #     try:
-    #         filters: dict[str, Any]
+#         """
+#         pass
