8ef2dde3242070f260134d89066ad194702646c0
Marcus <marcusrichardson7838@gmail.com>
2025-12-05 12:38:18 +0000

    added property decorator to user.py

--- data/classes/user.py
+++ data/classes/user.py
@@ -1,23 +1,23 @@
 from data.classes.book import Book
-from typing import Any
+from typing import Any, Optional
 
 
 class User:
     def __init__(
         self,
-        user_id: int,
+        user_id: Optional[int],
         first_name: str,
         last_name: str,
         email_address: str,
         phone_number: int,
         books_loaned: list[Book] | None = None,
     ) -> None:
-        self.user_id = user_id
+        self._user_id = user_id
         self.first_name = first_name
         self.last_name = last_name
-        self.email_address = email_address
-        self.phone_number = phone_number
-        self.books_loaned = books_loaned if books_loaned is not None else []
+        self._email_address = email_address
+        self._phone_number = phone_number
+        self._books_loaned = books_loaned if books_loaned is not None else []
 
     def __repr__(self) -> str:
         return f"User: {self.first_name} {self.last_name}, {len(self.books_loaned)} books loaned."
@@ -29,14 +29,36 @@ class User:
             "email_address": self.email_address,
         }
 
-    def surname_change(self, new_last_name: str) -> None:
+    @property
+    def user_id(self) -> Optional[int]:
+        return self._user_id
+
+    @property
+    def email_address(self) -> str:
+        return self._email_address
+
+    @email_address.setter
+    def email_address(self, new_email: str) -> None:
+        if not "@" in new_email:
+            raise ValueError("Email address must contain the @ symbol")
+        self._email_address = new_email
+
+    @property
+    def phone_number(self) -> int:
+        return self._phone_number
+
+    @phone_number.setter
+    def phone_number(self, new_phone_num: int) -> None:
+        if not isinstance(new_phone_num, int):  # type: ignore
+            raise TypeError("Phone number must be entered as an integer")
+        self._phone_number = new_phone_num
+
+    @property
+    def books_loaned(self) -> list[Book]:
+        return self._books_loaned
+
+    def update_last_name(self, new_last_name: str) -> None:
         self.last_name = new_last_name
 
-    def new_email(self, new_email: str) -> None:
-        self.email_address = new_email
-
-    def new_phone_number(self, new_phone_num: int) -> None:
-        self.phone_number = new_phone_num
-
-    def get_books_on_loan(self) -> int:
-        return len(self.books_loaned)
+    def get_loaned_books_amount(self) -> int:
+        return len(self._books_loaned)
