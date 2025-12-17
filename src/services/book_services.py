6ae19229e2dcc17dbf4a17f9a0f09d829ce699f9
Marcus <marcusrichardson7838@gmail.com>
2025-12-17 17:06:07 +0000

    Had some time to kill while waiting for the bus home. added logging to book services. Will do the other services over time

--- src/services/book_services.py
+++ src/services/book_services.py
@@ -18,34 +18,58 @@ class BookServices:
         logging.info("Attempting to create book: %s", self.book.title)
         try:
             self.book_queries.insert_book(self.book)
+            logging.info("Successfully created book: %s", self.book.title)
         except Exception as e:
             logging.exception("Failed to create book")
             raise DatabaseServiceError("Failed to create book") from e
 
     def get_book_details(self) -> list[dict[str, Any]] | None:
+        logging.info("Attempting to get book details for: %s", self.book.title)
         try:
-            rows = self.book_queries.find_by_title(self.book)
-            return rows
+            logging.info("Successfully retrieved book details for: %s", self.book.title)
+            return self.book_queries.find_by_title(self.book)
         except Exception as e:
+            logging.exception("Failed to get book details")
             raise DatabaseServiceError("Failed to get book details") from e
 
     def delete_book(self) -> None:
+        logging.info(
+            "Attempting to mark: %s as deleted in the database", self.book.title
+        )
         self.book.status = BookState.DELETED
         try:
             self.book_queries.update_book_status(self.book)
+            logging.info(
+                "Successfully marked: %s as deleted in the database", self.book.title
+            )
         except Exception as e:
+            logging.exception("Failed to mark book as deleted")
             raise DatabaseServiceError("Failed to mark book as deleted") from e
 
     def restore_book(self) -> None:
+        logging.info(
+            "Attempting to mark: %s as available in the database", self.book.title
+        )
         self.book.status = BookState.AVAILABLE
         try:
+            logging.info(
+                "Successfully marked: %s as available in the database", self.book.title
+            )
             self.book_queries.update_book_status(self.book)
         except Exception as e:
-            raise DatabaseServiceError("Failed to mark book as restored") from e
+            logging.exception("Failed to mark: %s as available", self.book.title)
+            raise DatabaseServiceError(
+                "Failed to mark: %s as available", self.book.title
+            ) from e
 
     def set_rating(self, new_rating: int) -> None:
+        logging.info("Attempting to update the rating of: %s", self.book.title)
         self.book.rating = new_rating
         try:
+            logging.info("Successfully updated rating for: %s", self.book.title)
             self.book_queries.update_rating(self.book)
         except Exception as e:
-            raise DatabaseServiceError("Failed to update book rating") from e
+            logging.exception("Failed to update book rating for: %s", self.book.title)
+            raise DatabaseServiceError(
+                "Failed to update book rating for: %s", self.book.title
+            ) from e
