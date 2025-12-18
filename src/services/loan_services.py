d43ca500abd83c5f7dd4bd778fdb010fffa31f05
Marcus <marcusrichardson7838@gmail.com>
2025-12-18 14:46:22 +0000

    added logging to loan services

--- src/services/loan_services.py
+++ src/services/loan_services.py
@@ -10,6 +10,7 @@ from src.services.exceptions import (
     DatabaseServiceError,
 )
 from typing import Any
+import logging
 
 
 class LoanServices:
@@ -28,44 +29,103 @@ class LoanServices:
         self.book = book
 
     def start_loan_transaction(self) -> None:
+        logging.info(
+            "Attempting to start loan transaction of %s for %s %s",
+            self.book.title,
+            self.user.first_name,
+            self.user.last_name,
+        )
         self.loan.borrow_book()
         try:
             self.loan_queries.insert_loan(self.book)
+            logging.info(
+                "Successfully initiated loan transactionfor %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
         except Exception as e:
+            logging.exception("Failed to start loan transaction")
             raise DatabaseServiceError("Failed to start loan transaction") from e
 
     def end_loan_transaction(self) -> None:
+        logging.info(
+            "Attempting to end loan transaction of %s for %s %s",
+            self.book.title,
+            self.user.first_name,
+            self.user.last_name,
+        )
         self.loan.return_book()
         try:
             self.loan_queries.end_loan(self.book)
+            logging.info("Successfully ended loan transaction")
         except Exception as e:
+            logging.exception("Failed to end loan transaction")
             raise DatabaseServiceError("Failed to end loan transaction") from e
 
     def get_loaned_books(self) -> list[dict[str, Any]] | None:
+        logging.info(
+            "Attempting to retrieve the books on loan for %s %s",
+            self.user.first_name,
+            self.user.last_name,
+        )
         try:
             self.loan_queries.get_books_on_loan(self.user)
+            logging.info(
+                "Successfully retrieved books on loan for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
         except Exception as e:
+            logging.exception(
+                "Failed to retrieve books on loan for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
             raise DatabaseServiceError(
-                "Failed to retrieve books on loan for user"
+                "Failed to retrieve books on loan for %s %s",
+                self.user.first_name,
+                self.user.last_name,
             ) from e
 
     def extend_loan_transaction(self) -> None:
+        logging.info("Attempting to extend loan transaction for %s", self.book.title)
         self.loan.extend_loan()
         try:
+            logging.info(
+                "Successfully extended loan transaction for %s", self.book.title
+            )
             self.loan_queries.loan_extension(self.loan)
         except Exception as e:
-            raise DatabaseServiceError("Failed to extend loan") from e
+            logging.exception("Failed to extend loan of %s", self.book.title)
+            raise DatabaseServiceError(
+                "Failed to extend loan of %s", self.book.title
+            ) from e
 
     def verify_loan_permissions(self) -> bool:
         """all verification stats can be created and then added to the verification module"""
+        logging.info(
+            "Check in progress to verify %s %s has permission to loan %s",
+            self.user.first_name,
+            self.user.last_name,
+            self.book.title,
+        )
         try:
             self.loan_queries.verification(self.user)
+            logging.info("Verification checks completed successfully")
         except Exception as e:
+            logging.exception(
+                "Failed to verify loan permissions for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
             raise DatabaseServiceError("Failed to verify loan permissions") from e
         return False
 
     def get_due_date(self) -> datetime | None:
+        logging.info("Attempting to get due date of %s", self.book.title)
         try:
             self.loan_queries.due_date_retrieval(self.book)
+            logging.info("Successfully retrieved due date for %s", self.book.title)
         except Exception as e:
+            logging.exception("Failed to retrive due date of %s", self.book.title)
             raise DatabaseServiceError("Failed to retrieve due_date") from e
