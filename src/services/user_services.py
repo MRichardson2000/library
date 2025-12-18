362c8ef7af29dd4020e8b091e36120ea22986f23
Marcus <marcusrichardson7838@gmail.com>
2025-12-18 15:19:38 +0000

    Added logging to user services

--- src/services/user_services.py
+++ src/services/user_services.py
@@ -6,6 +6,7 @@ from src.services.exceptions import (
     DatabaseServiceError,
 )
 from typing import Any
+import logging
 
 
 class UserServices:
@@ -14,54 +15,135 @@ class UserServices:
         self.user = user
 
     def create_user(self) -> None:
+        logging.info("Attempting to create user")
         try:
             self.user_queries.insert_user(self.user)
+            logging.info("Successfully created user")
         except Exception as e:
+            logging.exception("Failed to create user")
             raise DatabaseServiceError("Failed to create user") from e
 
     def get_user_details(self) -> dict[str, Any] | None:
+        logging.info("Attempting to get user details from the database")
         try:
             rows = self.user_queries.find_by_first_name(self.user)
             if rows:
+                logging.info("Successfully retrieved user details")
                 return rows[0]
         except Exception as e:
-            raise DatabaseServiceError("Failed to get user details") from e
+            logging.exception("Failed to get user details from the database")
+            raise DatabaseServiceError(
+                "Failed to get user details from the database"
+            ) from e
 
     def change_surname(self, new_surname: str) -> None:
+        logging.info("Attempting to change surname for %s", self.user.first_name)
         self.user.last_name = new_surname
         try:
             self.user_queries.set_surname(self.user)
+            logging.info("Successfully changed surname for %s", self.user.first_name)
         except Exception as e:
-            raise DatabaseServiceError("Failed to change surname") from e
+            logging.exception("Failed to change surname for %s", self.user.first_name)
+            raise DatabaseServiceError(
+                "Failed to change surname for %s", self.user.first_name
+            ) from e
 
     def email_change(self, new_email_address: str) -> None:
+        logging.info(
+            "Attempting to change email address for %s %s",
+            self.user.first_name,
+            self.user.last_name,
+        )
         self.user.email_address = new_email_address
         try:
             self.user_queries.set_email(self.user)
+            logging.info(
+                "Successfully changed email address for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
         except Exception as e:
-            raise DatabaseServiceError("Failed to change email") from e
+            logging.exception
+            raise DatabaseServiceError(
+                "Failed to change email address for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            ) from e
 
     def phone_number_change(self, new_phone_number: str) -> None:
+        logging.info(
+            "Attempting to update phone number for %s %s",
+            self.user.first_name,
+            self.user.last_name,
+        )
         self.user.phone_number = new_phone_number
         try:
             self.user_queries.set_phone_number(self.user)
+            logging.info(
+                "Successfully changed phone number for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
         except Exception as e:
-            raise DatabaseServiceError("Failed to change phone_number") from e
+            logging.exception(
+                "Failed to change phone number for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
+            raise DatabaseServiceError(
+                "Failed to change phone_number for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            ) from e
 
     def delete_user(self) -> None:
+        logging.info(
+            "Attempting to mark %s %s as deleted in the database",
+            self.user.first_name,
+            self.user.last_name,
+        )
         self.user.account_state = AccountState.DELETED
         try:
             self.user_queries.set_status(self.user)
+            logging.info(
+                "Successfully changed the status to deleted for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
         except Exception as e:
+            logging.exception(
+                "Failed to change account_state to deleted for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
             raise DatabaseServiceError(
-                "Failed to change account_state to deleted"
+                "Failed to change account_state to deleted for %s %s",
+                self.user.first_name,
+                self.user.last_name,
             ) from e
 
     def restore_user(self) -> None:
+        logging.info(
+            "Attempting to mark %s %s as Active in the database",
+            self.user.first_name,
+            self.user.last_name,
+        )
         self.user.account_state = AccountState.ACTIVE
         try:
             self.user_queries.set_status(self.user)
+            logging.info(
+                "Successfully marked %s %s as Active in the database",
+                self.user.first_name,
+                self.user.last_name,
+            )
         except Exception as e:
+            logging.exception(
+                "Failed to change account_state to active for %s %s",
+                self.user.first_name,
+                self.user.last_name,
+            )
             raise DatabaseServiceError(
-                "Failed to change account_state to active"
+                "Failed to change account_state to active for %s %s",
+                self.user.first_name,
+                self.user.last_name,
             ) from e
