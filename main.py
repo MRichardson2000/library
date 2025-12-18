ee1c717eb9afb116138fd90b0895c972b24cbfe8
Ian de Rijk <ian.derijk@gmail.com>
2025-12-18 22:23:28 +0000

    changes at ians

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from data.classes.book import Book
+from data.classes.user import User
+
+def two_csutomer_loan_a_booK() -> NOne:  
+    cust1 = User("test", "test", "test@test.test.test", "012345678")
+    cust2 = User("test1", "test1", "test1@test.test.test", "123456789")
+    
+   
+book1 = Book("test", "test", "test", 0.0)
+
 
 
 def testing_repr() -> Book:
@@ -12,3 +21,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+'''
+write some code that allows you to instantiate two users that each loan out one book both return their books.
+'''
